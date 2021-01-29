002b7218d5cb8644fe22bbdad01f1be0a7691700
Florian <florian.gacon@ifsttar.fr>
2021-01-29 13:27:58 +0100

    First working draft MacOS

---
+++ conda/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ -d build ]]; then
+    rm -rf build
+fi
+
+mkdir build
+cd build
+
+cmake .. \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_PREFIX_PATH="${PREFIX}" \
+      -DCMAKE_INSTALL_LIBDIR=lib
+
+make -j${CPU_COUNT}
+make install
